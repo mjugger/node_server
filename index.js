79fbda1fe20d59de775ff2c597366131ee094044
mjugger <xaiox20@gmail.com>
2014-08-21 22:53:28 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http'),
+	os = require('os');
+
+http.createServer(function(req,res){
+	res.writeHead(200,{'content-type':'text/html'});
+	res.end('<!DOCTYPE html><html><body><p>Hi!<br/>My name is '+os.hostname()+'. I am your computer.</p></body></html>');
+	console.log(req.url);
+}).listen(3000,'localhost');
+
+console.log('server is running');
